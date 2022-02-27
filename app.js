58be78b1070fe8a1c3d416d8221985db4be794a1
betulkaraduman <betulkrdmnn@hotmail.com>
2022-02-27 18:03:51 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-const fn = function () {
-  console.log("Test");
-};
+const express = require("express");
+const path  = require('path');
+const app = express();
+const PORT = 3000;
+
+//Middleware
+const myMiddleware=(req,res,next)=>{
+  console.log('Middleware Log 1')
+  next();
+}
+app.use(express.static('public'))
+//app.use(myMiddleware);
+
+app.get('/',(req,res)=>{
+const photos={id:1,name :'Phota 1',description:'Description 1'}
+res.sendFile(path.resolve(__dirname,'temp/index.html'))
+})
+
+app.listen(PORT, () => {
+  console.log(`Server started ${PORT}`);
+});
